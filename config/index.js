7801c410b5f2b2c2f0d8eafa5cddb92196aaf5f2
liumapp <liumapp.com@gmail.com>
2018-04-13 17:10:44 +0800

    add static hello dem o

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api/v1/': {
+        target: 'http://127.0.0.1:8080/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/v1/': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -20,7 +28,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,84 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li><a v-on:click="sayHello">hello</a></li>
+      <li><a v-on:click="getHello">get hello from api</a></li>
     </ul>
   </div>
 </template>
@@ -86,10 +11,28 @@
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data: function () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods: {
+
+    sayHello: function sayHello () {
+      this.msg = 'hello there '
+    },
+
+    getHello: function getHello () {
+      return $.ajax({
+        type: 'GET',
+        url: '/api/v1/',
+        dataType: 'json',
+        success: function (data) {
+          console.log(data)
+        }
+      });
+    }
+
   }
 }
 </script>
