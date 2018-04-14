0c2e51edfb2f49a422eef51fc7524c14be942962
liumapp <liumapp.com@gmail.com>
2018-04-14 15:13:19 +0800

    add remote config

--- config/index.js
+++ config/index.js
@@ -44,7 +44,43 @@ module.exports = {
     cssSourceMap: true
   },
 
+  remote: {
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/api/v1/': {
+        target: 'http://www.example.com:8080/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/v1/': ''
+        }
+      }
+    },
 
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 3000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map',
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    cssSourceMap: true
+  },
 
   build: {
     // Template for index.html
